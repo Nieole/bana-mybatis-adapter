43d525f58fe1c9a395228553963760e0ba45fe76
nicoer <saber00sword@gmail.com>
2021-08-19 09:55:53 +0800

    rename

--- src/main/java/org/bana/mybatis/entity/MybatisRule.java
+++ src/main/java/org/bana/mybatis/entity/MybatisRule.java
@@ -1,8 +1,10 @@
 package org.bana.mybatis.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import org.bana.entity.CasbinRule;
 
+@TableName(value = "rule")
 public class MybatisRule extends CasbinRule<String> {
   @TableId
   private String id;
